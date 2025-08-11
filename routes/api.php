d9ee0ae99cb072cd92db7b65b98cd084a213d38d
Abdullah Ahmed <abdullah.ah452001@gmail.com>
2025-08-11 21:13:06 +0500

    setup

--- routes/api.php
+++ routes/api.php
@@ -3,11 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
-// Public routes
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
-// Protected routes
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/me', [AuthController::class, 'me']);
     Route::post('/logout', [AuthController::class, 'logout']);
